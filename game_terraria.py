9aebbd6012c96f47b7010caae101668e936d43a8
Drazden <hacodaro@gmail.com>
2021-10-16 19:34:56 -0400

    Add files via upload

---
+++ game_terraria.py
@@ -0,0 +1,19 @@
+from ..basic_game import BasicGame
+
+
+class Terraria(BasicGame):
+
+    Name = "Terraria"
+    Author = "Drazden"
+    Version = "1.0"
+
+    GameName = "Terraria"
+    GameShortName = "terraria"
+    GameValidShortNames = ["terraria", "tModLoader"]
+    GameBinary = "tModLoader.exe"
+    GameDataPath = "%DOCUMENTS%/My Games/Terraria/ModLoader/Mods"
+    GameSavesDirectory = "%DOCUMENTS%/My Games/Terraria/ModLoader/Players"
+    GameSaveExtension = "tplr" 
+    GameSteamId = 1281930
+    GameNexusName = "terraria"
+    GameNexusId = 549
